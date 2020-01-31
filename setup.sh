ab0d864ebb0a0676cc5e4c4e71feb7db1c5c1070
MarkGillespie <mark.gillespie81@gmail.com>
2020-01-30 20:27:09 -0500

    Update setup.sh
    Changed to use master branch of geometry-central

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 mkdir deps
 cd deps
 git submodule add https://github.com/MarkGillespie/polyscope.git
-git submodule add -b v1 https://github.com/MarkGillespie/geometry-central.git
+git submodule add https://github.com/MarkGillespie/geometry-central.git
 git submodule add https://github.com/google/googletest
 git submodule update --init --recursive
 cd ..
