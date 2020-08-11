d32dc61f0e70f9df2d3627aee299fad5f02b0b33
Mark Gillespie <mark.gillespie81@gmail.com>
2020-08-11 17:00:37 -0400

    Fix submodule github links

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 mkdir deps
 cd deps
-git submodule add git@github.com/MarkGillespie/polyscope.git
-git submodule add git@github.com/MarkGillespie/geometry-central.git
-git submodule add git@github.com/google/googletest
+git submodule add git@github.com:MarkGillespie/polyscope.git
+git submodule add git@github.com:MarkGillespie/geometry-central.git
+git submodule add git@github.com:google/googletest
 git submodule update --init --recursive
 cd ..
 mkdir build
